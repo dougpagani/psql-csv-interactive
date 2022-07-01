1c3cd10cdde7fdd42b0ecf856f05544f577e6a7e
dougpagani <redstonefreedom@gmail.com>
2022-07-01 01:10:05 -0400

    Refactor: make install line a commentable multiline array

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 ########################################
 cat >>~/.psqlrc <<'PSQLRC'
-\ir '.psql-csv-interactive'
+\ir './.psql-csv-interactive'
 PSQLRC
 
 thisdir=$(dirname "$0")
 
-ln -s "$thisdir"/psql-csv-interactive.sql ~/.psql-csv-interactive
+install=(
+ln -s 
+    .psqlrc-csv-interactive
+    "$thisdir"/psql-csv-interactive.sql
+)
+
+"${install[@]}"
 
